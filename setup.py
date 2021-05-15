8a9ea5f95ef7ad265d135217cfbbcf44401264dc
Rob Stenson <rob.stenson@gmail.com>
2021-05-15 15:13:23 -0700

    bump to 0.1.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Color and geometric primitives
 
 setuptools.setup(
     name="drafting",
-    version="0.1.6",
+    version="0.1.7",
     author="Rob Stenson / Goodhertz",
     author_email="rob@goodhertz.com",
     description="Color and geometric primitives",
