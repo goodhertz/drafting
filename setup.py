8648732ea1bb204878f2519db9eb1ab2c6704267
Rob Stenson <rob.stenson@gmail.com>
2021-05-27 10:20:19 -0700

    bump to 0.2.4 for time classes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Color and geometric primitives
 
 setuptools.setup(
     name="drafting",
-    version="0.2.3",
+    version="0.2.4",
     author="Rob Stenson / Goodhertz",
     author_email="rob@goodhertz.com",
     description="Color and geometric primitives",
