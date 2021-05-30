8d91ce151d46d8eab0bb6e224148b84b18bfe223
Rob Stenson <rob.stenson@gmail.com>
2021-05-30 14:44:27 -0700

    delete dead text __init__

--- drafting/text/__init__.py
+++
@@ -1,2 +0,0 @@
-#from drafting.text.reader import Style, StyledString, SegmentedString, normalize_font_path, Font, FontNotFoundException
-#from drafting.text.composer import Slug, Lockup, Graf, GrafStyle, T2L, Composer
