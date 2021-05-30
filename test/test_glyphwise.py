d125df88490d24a8e6be55afa181b14b62476495
Rob Stenson <rob.stenson@gmail.com>
2021-05-30 14:38:47 -0700

    tidy up glyphwise test

--- test/test_glyphwise.py
+++ test/test_glyphwise.py
@@ -2,13 +2,14 @@ import unittest
 from pathlib import Path
 from drafting.grid import Grid
 from drafting.geometry import *
-from drafting.text.composer import StSt, Glyphwise, Style
+from drafting.text.composer import StSt, Glyphwise, Style, Font
+from drafting.pens.draftingpens import DraftingPens
 from drafting.pens.svgpen import SVGPen
 
 tf = Path(__file__).parent
 
 class TestGlyphwise(unittest.TestCase):
-    def _test_glyph_names(self, font_path, kern):
+    def _test_glyph_kerning(self, font_path, kern):
         ss = StSt("AVAMANAV", font_path, 100, wdth=0, kern=kern)
         gw = Glyphwise("AVAMANAV", lambda i, c: Style(font_path, 100, wdth=0, kern=kern))
         gwo = Glyphwise("AVAMANAV", lambda i, c: Style(font_path, 100, wdth=0, kern=(not kern)))
@@ -26,16 +27,13 @@ class TestGlyphwise(unittest.TestCase):
         fp = Path(font_path)
         op = (tf / f"ignorables/__{fp.name}.svg")
         op.parent.mkdir(exist_ok=True)
-        op.write_text(SVGPen.Composite(ss, ss.ambit(), viewBox=True))
+        op.write_text(SVGPen.Composite(DraftingPens([ss, gw.translate(0, 10)]), ss.ambit(), viewBox=True))
         return ss, gw
 
     def test_format_equality(self):
-        self._test_glyph_names("~/Type/fonts/fonts/OhnoFatfaceVariable.ttf", True)
-        self._test_glyph_names("~/Type/fonts/fonts/OhnoFatfaceVariable.ttf", False)
-
-        # TODO why isn't the ttf version equal to these?
-        #self.assertEqual(ufo[0].value, ds[0].value)
-        #self.assertEqual(ufo[-1].value, ds[-1].value)
+        fnt = "~/Type/fonts/fonts/OhnoFatfaceVariable.ttf"
+        self._test_glyph_kerning(fnt, True)
+        self._test_glyph_kerning(fnt, False)
 
 if __name__ == "__main__":
     unittest.main()
